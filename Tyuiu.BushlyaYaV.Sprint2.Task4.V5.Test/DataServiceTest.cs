73139b5b1f213a01871165a44686fb3774607813
Tyuiu.BushljaJV@outlook.com <Tyuiu.BushljaJV@outlook.com>
2024-10-22 18:18:16 +0500

    Task4

---
+++ Tyuiu.BushlyaYaV.Sprint2.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+
+
+using Tyuiu.BushlyaYaV.Sprint2.Task4.V5.Lib;
+namespace Tyuiu.BushlyaYaV.Sprint2.Task4.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(12, ds.Calculate(1, 1));
+        }
+    }
+}
---
+++ Tyuiu.BushlyaYaV.Sprint2.Task4.V5/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Tyuiu.BushlyaYaV.Sprint2.Task4.V5.Lib;
+namespace Tyuiu.BushlyaYaV.Sprint2.Task4.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Бушля Я. В. | ИСТНб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тернарный оператор                                                      *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Бушля Ярослав Владимирович | ИСТНб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу.                                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            double a, b;
+
+            Console.Write("Введите значение переменной X: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение переменной Y: ");
+
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+           
+            
+            
+            var result = ds.Calculate(a, b);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
