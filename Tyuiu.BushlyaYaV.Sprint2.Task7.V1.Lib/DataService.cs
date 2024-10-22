9e1edfbd0fe9de097ad95f09cd9c150e1d4f34b1
Tyuiu.BushljaJV@outlook.com <Tyuiu.BushljaJV@outlook.com>
2024-10-22 21:58:32 +0500

    Task7

--- Tyuiu.BushlyaYaV.Sprint2.Task7.V1.Lib/DataService.cs
+++ Tyuiu.BushlyaYaV.Sprint2.Task7.V1.Lib/DataService.cs
@@ -9,9 +9,9 @@ namespace Tyuiu.BushlyaYaV.Sprint2.Task7.V1.Lib
         {
             bool res;
             if ((y > x) && (y > -x) && (y < Math.Sqrt(1 - Math.Pow(x, 2))))
-                res = true;
-            else
                 res = false;
+            else
+                res = true;
             return res;
         }
     }
